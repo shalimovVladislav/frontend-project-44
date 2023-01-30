2d542045ed22439fe2414890b6752789410f48a3
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-01-30 21:24:47 +0400

    add module brain-gcd.js

---
+++ src/brain-games/brain-gcd.js
@@ -0,0 +1,13 @@
+const GetGreatestCommonDivisor = (num1, num2) => {
+  let a = num1;
+  let b = num2;
+  while ((a !== 0) && (b !== 0)) {
+    if (a > b) {
+      a %= b;
+    } else {
+      b %= a;
+    }
+  }
+  return String(a + b);
+};
+export default GetGreatestCommonDivisor;

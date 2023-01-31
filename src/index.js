8286baf484f377fd1ab63c43046f20a5443da7f3
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-01-31 22:07:58 +0400

    add index.js

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import Greeting from './cli.js';
+import BrainEven from './brain-games/brain-even.js';
+import BrainCalc from './brain-games/brain-calc.js';
+import BrainGCD from './brain-games/brain-gcd.js';
+
+const StartBrainGame = (gametype) => {
+  const username = Greeting();
+  switch (gametype) {
+    case 'even':
+      console.log('Answer "yes" if the number is even, otherwise answer "no".');
+      break;
+    case 'calc':
+      console.log('What is the result of the expression?');
+      break;
+    case 'gcd':
+      console.log('Find the greatest common divisor of given numbers.');
+      break;
+    default:
+      break;
+  }
+  for (let i = 0; i < 3; i += 1) {
+    switch (gametype) {
+      case 'even':
+        if (BrainEven(username)) {
+          return;
+        }
+        break;
+      case 'calc':
+        if (BrainCalc(username)) {
+          return;
+        }
+        break;
+      case 'gcd':
+        if (BrainGCD(username)) {
+          return;
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  console.log(`Congratulations, ${username}!`);
+};
+export default StartBrainGame;

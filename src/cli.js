8a58eb871e47e178cc6f60a2c3b12051b66bdd13
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-01-31 22:09:50 +0400

    fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,12 +1,13 @@
 import readlineSync from 'readline-sync';
 
-export const GetUsername = () => {
+const GetUsername = () => {
   const name = readlineSync.question('May I have your name? ');
   return name;
 };
-export const Greeting = () => {
+const Greeting = () => {
   console.log('Welcome to the Brain Games!');
   const Username = GetUsername();
   console.log(`Hello, ${Username}!`);
   return Username;
 };
+export default Greeting;

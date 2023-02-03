01ca271f2065fde0fc719990874cbd8122af1ba5
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-02-03 17:01:19 +0400

    fix isPrime predicate function

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -5,14 +5,14 @@ const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const isPrime = (number) => {
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
-      return 'no';
+      return false;
     }
   }
-  return 'yes';
+  return true;
 };
 const brainPrime = () => {
   const randomNumber = getRandom(2);
-  const correctAnswer = isPrime(randomNumber);
+  const correctAnswer = (isPrime(randomNumber) ? 'yes' : 'no');
   const questionAndCorrectAnswer = [randomNumber, correctAnswer];
   return questionAndCorrectAnswer;
 };

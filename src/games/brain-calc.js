36ed8f8fcd95ed0101264f94a508c8d7581b3f48
vladislav_shalimov <shalimov-vlad132@mail.ru>
2023-02-03 16:44:17 +0400

    fixed sign finding

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -2,24 +2,7 @@ import getRandom from '../getRandom.js';
 import startBrainGame from '../index.js';
 
 const rules = 'What is the result of the expression?';
-const getSign = () => {
-  let sign = null;
-  const randomNumber = getRandom(0, 3);
-  switch (randomNumber) {
-    case 0:
-      sign = '+';
-      break;
-    case 1:
-      sign = '-';
-      break;
-    case 2:
-      sign = '*';
-      break;
-    default:
-      break;
-  }
-  return sign;
-};
+const operators = ['+', '-', '*'];
 const getResultOfExpression = (num1, num2, operationSign) => {
   let result = null;
   switch (operationSign) {
@@ -39,7 +22,7 @@ const getResultOfExpression = (num1, num2, operationSign) => {
 const brainCalc = () => {
   const randomNumber1 = getRandom();
   const randomNumber2 = getRandom();
-  const operationSign = getSign();
+  const operationSign = operators[getRandom(0,3)];
   const question = `${randomNumber1} ${operationSign} ${randomNumber2}`;
   const correctAnswer = getResultOfExpression(randomNumber1, randomNumber2, operationSign);
   const questionAndCorrectAnswer = [question, correctAnswer];
